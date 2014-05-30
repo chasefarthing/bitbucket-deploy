14e63975226a44af3377fb67e498e1b99e2535f4
Chase <chasefarthing@me.com>
2014-05-29 17:22:34 -0700

    init

---
+++ deploy-bitbucket.php
@@ -0,0 +1,40 @@
+<?php
+
+$repo_dir = '/nfs/c08/h02/mnt/122813/domains/frank.chasefarthing.com/frank-website.git';
+$web_root_dir = '/nfs/c08/h02/mnt/122813/domains/frank.chasefarthing.com/html';
+ 
+// Full path to git binary is required if git is not in your PHP user's path. Otherwise just use 'git'.
+$git_bin_path = 'git';
+ 
+$update = false;
+ 
+// Parse data from Bitbucket hook payload
+$payload = json_decode(stripslashes($_POST['payload']));
+
+
+ 
+if (empty($payload->commits)){
+  // When merging and pushing to bitbucket, the commits array will be empty.
+  // In this case there is no way to know what branch was pushed to, so we will do an update.
+  $update = true;
+} else {
+  foreach ($payload->commits as $commit) {
+    $branch = $commit->branch;
+    if ($branch === 'master' || isset($commit->branches) && in_array('master', $commit->branches)) {
+      $update = true; // Instead of update add another if for dev and master
+      break;
+    }
+  }
+}
+ 
+if ($update) {
+  // Do a git checkout to the web root
+  exec('cd ' . $repo_dir . ' && ' . $git_bin_path  . ' fetch');
+  exec('cd ' . $repo_dir . ' && GIT_WORK_TREE=' . $web_root_dir . ' ' . $git_bin_path  . ' checkout -f master');
+ 
+  // Log the deployment
+  $commit_hash = shell_exec('cd ' . $repo_dir . ' && ' . $git_bin_path  . ' rev-parse --short HEAD');
+  file_put_contents('deploy.log', date('m/d/Y h:i:s a') . " Deployed branch: " . $branch . " Commit: " . $commit_hash . "\n", FILE_APPEND);
+ 
+}
+?>
